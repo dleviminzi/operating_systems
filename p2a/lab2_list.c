08b726bcff2a5181a23a1c931e0d6d366a2e5f3f
Daniel Levi-Minzi <dleviminzi@gmail.com>
2020-11-11 17:14:28 -0800

    actual fix

--- p2a/lab2_list.c
+++ p2a/lab2_list.c
@@ -140,8 +140,6 @@ int main(int argc, char *argv[]) {
                 } 
                 break;
             case 's':
-                syncing = 1;
-
                 switch(optarg[0]) {
                     case 'm':
                         mutexFlg = 1;
