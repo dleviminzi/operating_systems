29908741e655deb6ef4a952b105433651889d324
Daniel Levi-Minzi <dleviminzi@gmail.com>
2020-11-17 13:57:32 -0800

    pls

--- p2b/test.sh
+++ p2b/test.sh
@@ -1,21 +1,21 @@
-rm -f lab2b_list.csv
-touch lab2b_list.csv
+rm -f lab2_list.csv
+touch lab2_list.csv
 
 for i in 1, 2, 4, 8, 12, 16, 24
 do 
-    ./lab2b_list --threads=$i --sync=m --iterations=1000 >> lab2b_list.csv
+    ./lab2_list --threads=$i --sync=m --iterations=1000 >> lab2b_list.csv
 done
 
 for i in 1, 2, 4, 8, 12, 16, 24
 do
-    ./lab2b_list --threads=$i --sync=s --iterations=1000 >> lab2b_list.csv
+    ./lab2_list --threads=$i --sync=s --iterations=1000 >> lab2b_list.csv
 done
 
 for i in 1, 4, 8, 12, 16
 do 
     for j in 1, 2, 4, 8, 16
     do
-        ./lab2b_list --threads=$i --iterations=$j --yield=id --lists=4 >> lab2b_list.csv
+        ./lab2_list --threads=$i --iterations=$j --yield=id --lists=4 >> lab2b_list.csv
     done
 done
 
@@ -23,7 +23,7 @@ for i in 1, 4, 8, 12, 16
 do 
     for j in 10, 20, 40, 80
     do
-        ./lab2b_list --threads=$i --iterations=$j --sync=m --yield=id --lists=4 >> lab2b_list.csv
+        ./lab2_list --threads=$i --iterations=$j --sync=m --yield=id --lists=4 >> lab2b_list.csv
     done
 done
 
@@ -31,7 +31,7 @@ for i in 1, 4, 8, 12, 16
 do 
     for j in 10, 20, 40, 80
     do
-        ./lab2b_list --threads=$i --iterations=$j --sync=s --yield=id --lists=4 >> lab2b_list.csv
+        ./lab2_list --threads=$i --iterations=$j --sync=s --yield=id --lists=4 >> lab2b_list.csv
     done
 done
 
@@ -39,7 +39,7 @@ for i in 1, 2, 4, 8, 12, 16
 do 
     for j in 4, 8, 16
     do
-        ./lab2b_list --threads=$i --iterations=1000 --yield=id --lists=$j --sync=m >> lab2b_list.csv
+        ./lab2_list --threads=$i --iterations=1000 --yield=id --lists=$j --sync=m >> lab2b_list.csv
     done
 done
 
@@ -47,6 +47,6 @@ for i in 1, 2, 4, 8, 12, 16
 do 
     for j in 4, 8, 16
     do
-        ./lab2b_list --threads=$i --iterations=1000 --yield=id --lists=$j --sync=s >> lab2b_list.csv
+        ./lab2_list --threads=$i --iterations=1000 --yield=id --lists=$j --sync=s >> lab2b_list.csv
     done
 done
