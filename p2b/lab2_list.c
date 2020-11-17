5b368d8c8c90c2d3ef883f2f233bb684fe3bf6db
Daniel Levi-Minzi <dleviminzi@gmail.com>
2020-11-16 19:40:12 -0800

    testing on server

--- p2b/lab2_list.c
+++ p2b/lab2_list.c
@@ -15,40 +15,36 @@
 #define SUCCESS 0
 #define BILLION 1E9
 
-/* list and elements */
-SortedList_t *list;
-SortedListElement_t *elements;
+/* globale variables */
+SortedListElement_t *elements;      /* elements for lists */
+SortedListElement_t *lists;         /* list heads */
+pthread_mutex_t *mutexLocks;        /* mutex locks for lists */
+int *spinLocks;                     /* spin locks for lists */
+int opt_yield = 0;          /* yield option specifier */
+int mutexFlg = 0;           /* mutex lock flag */
+int spinFlg = 0;            /* spin lock flag */
+int profileFlg = 0;         /* profiling flag */
 
-/* yield option specifier */
-int opt_yield = 0;
-
-/* mutex lock parameters */
-pthread_mutex_t mutexLock;
-int mutexFlg = 0;
-
-/* spin lock */
-int spinlock = 0;
-int spinFlg = 0;
 
 /* struct to pass arguments to thread calling function */
 struct threadArgs {
     int threadNum;
-    int *numThreads;
-    int *numElements;
+    int numThreads;
+    int numElements;
     long long *waitTime;
+    int numLists;
+    pthread_mutex_t **mutexLocks;
+    int **spinLocks;
 };
 
-/* profiling flag */
-int profileFlg = 0;
-
 /* method to lock thread when needed */
-long lock() {
+long lock(pthread_mutex_t *mutexLock, int *spinLock) {
     struct timespec startTime, endTime;
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &startTime);
 
-    if (mutexFlg) pthread_mutex_lock(&mutexLock);
+    if (mutexFlg) pthread_mutex_lock(mutexLock);
     else if (spinFlg) {
-        while (__sync_lock_test_and_set(&spinlock, 1));
+        while (__sync_lock_test_and_set(spinLock, 1));
     }
 
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &endTime);
@@ -57,9 +53,9 @@ long lock() {
 }
 
 /* method to unlock thread when needed */
-void unlock() {
-    if (mutexFlg) pthread_mutex_unlock(&mutexLock);
-    else if (spinFlg) __sync_lock_release(&spinlock);
+void unlock(pthread_mutex_t *mutexLock, int *spinLock) {
+    if (mutexFlg) pthread_mutex_unlock(mutexLock);
+    else if (spinFlg) __sync_lock_release(spinLock);
 }
 
 /* function to be called when threading */
@@ -68,36 +64,49 @@ void *threadCall(void *threadArgs) {
     currArgs = (struct threadArgs *) threadArgs;
 
     int threadNum = currArgs->threadNum;
-    int numThreads = *(currArgs->numThreads);
-    int numElements = *(currArgs->numElements);
+    int numThreads = currArgs->numThreads;
+    int numElements = currArgs->numElements;
     long long *waitTime = currArgs->waitTime;
+    int numLists = currArgs->numLists;
 
     /* inserting elements into the list */
     int i;
     for (i = threadNum; i < numElements; i += numThreads) {
-        *waitTime += lock();
-        SortedList_insert(list, &elements[i]);
-        unlock();
+        /* determine which list the element will be inserted into */
+        int listToIns;
+        listToIns = (*elements[i].key)%numLists;
+
+        *waitTime += lock(&mutexLocks[listToIns], &spinLocks[listToIns]);
+        SortedList_insert(&lists[listToIns], &elements[i]);
+        unlock(&mutexLocks[listToIns], &spinLocks[listToIns]);
     }
 
-    /* check size of list */
-    *waitTime += lock();
 
     int length = 0;
-    length = SortedList_length(list);
+    for (i = 0; i < numLists; ++i) {
+        /* lock all lists so we can get the length */
+        *waitTime += lock(&mutexLocks[i], &spinLocks[i]);
+
+        length += SortedList_length(&lists[i]);
+
+        /* unlock all the lists now that we've counted it */
+        unlock(&mutexLocks[i], &spinLocks[i]);
+    }
+
     if (length < 0) {
         fprintf(stderr, "Failed to get length of list: corruption.\n");
         exit(ERROR2);
     }
 
-    unlock();
-
     /* lookup and delete elements that were added to list */
     for (i = threadNum; i < numElements; i += numThreads) {
-        *waitTime += lock();
+        int listToIns;
+        listToIns = (*elements[i].key)%numLists;
+
+        *waitTime += lock(&mutexLocks[listToIns], &spinLocks[listToIns]);
 
         SortedListElement_t *element;
-        if ((element = SortedList_lookup(list, elements[i].key)) == NULL) {
+        if ((element = SortedList_lookup(&lists[listToIns], elements[i].key)) == NULL) {
             fprintf(stderr, "Could not find element in list: corruption.\n");
             exit(ERROR2);
         }
@@ -107,7 +116,7 @@ void *threadCall(void *threadArgs) {
             exit(ERROR2);
         }
 
-        unlock();
+        unlock(&mutexLocks[listToIns], &spinLocks[listToIns]);
     } 
 
     pthread_exit(NULL);
@@ -150,6 +159,7 @@ int main(int argc, char *argv[]) {
         {"sync", required_argument, NULL, 's'},
         {"yield", required_argument, NULL, 'y'},
         {"profile", no_argument, &profileFlg, 1},
+        {"lists", required_argument, NULL, 'l'},
         {0,0,0,0}
     };    
 
@@ -160,6 +170,7 @@ int main(int argc, char *argv[]) {
     int numThreads = 1;
     int numIterations = 1;
     int numElements = 1;
+    int numLists = 1;
 
     char *yldOpts;
 
@@ -171,6 +182,9 @@ int main(int argc, char *argv[]) {
                 break;
             case 'i':
                 numIterations = atoi(optarg);
+                break;
+            case 'l':
+                numLists = atoi(optarg);
                 break;
             case 'y':
                 opt_yield = 1;
@@ -215,27 +229,38 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    /* check for/initialize mutex lock */
-    if (mutexFlg) {
-        pthread_mutex_init(&mutexLock, NULL);
-    }
-
     /* check for/start profiling */
     if (profileFlg) {
         ProfilerStart("profile.prof");
     }
 
-    /* init list of elements */
-    list = (SortedList_t *) malloc(sizeof(SortedList_t)); 
-    list->next = list;
-    list->prev = list;
-    list->key = NULL;
+    /* initialize array of pointers to list heads */
+    lists = (SortedListElement_t *) malloc(sizeof(SortedListElement_t) * numLists);
+
+    int i;
+    for (i = 0; i < numLists; ++i) {        
+        lists[i].next = &lists[i];
+        lists[i].prev = &lists[i];
+        lists[i].key = NULL;
+    }
+
+    /* create arrays of locks, each corresponding to a list head */
+    mutexLocks = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t) * numLists);
+    spinLocks = (int *) malloc(sizeof(int)* numLists);
+
+    for (i = 0; i < numLists; ++i) {
+        if (mutexFlg) {
+            pthread_mutex_init(&mutexLocks[i], NULL);
+        }
+        else if (spinFlg) {
+            spinLocks[i] = 0;
+        }
+    }
 
     /* init elements */
     numElements = numIterations * numThreads;
     elements = (SortedListElement_t*) malloc(sizeof(SortedListElement_t) * numElements); 
 
-    int i;
     for (i = 0; i < numElements; ++i) {
         char key[15];
         randomKey(key);
@@ -263,9 +288,10 @@ int main(int argc, char *argv[]) {
 
         /* argument to be passed with function jump point */
         tArgs[t].threadNum = t;
-        tArgs[t].numElements = &numElements;
-        tArgs[t].numThreads = &numThreads;
+        tArgs[t].numElements = numElements;
+        tArgs[t].numThreads = numThreads;
         tArgs[t].waitTime = &waitTimes[t];
+        tArgs[t].numLists = numLists;
 
         if ((rc = pthread_create(&threads[t], NULL, threadCall, (void *) &tArgs[t])) < 0) {
             fprintf(stderr, "Thread creation failed with error code: %d\n", rc);
@@ -286,12 +312,18 @@ int main(int argc, char *argv[]) {
     /* get ending time */
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &endTime);
 
-    if (mutexFlg) {
-        pthread_mutex_destroy(&mutexLock);
-    } 
+    int finalLength = 0;
+
+    for (i = 0; i < numLists; ++i) {
+        if (mutexFlg) {
+            pthread_mutex_destroy(&mutexLocks[i]);
+        }
+
+        finalLength += SortedList_length(&lists[i]);
+    }
 
     /* checking length of list */
-    if (SortedList_length(list) != 0) {
+    if (finalLength != 0) {
         fprintf(stderr, "List was corrupted.");
         exit(ERROR2);
     }
@@ -307,7 +339,9 @@ int main(int argc, char *argv[]) {
     }
 
     free(elements);
-    free(list);
+    free(lists);
+    free(mutexLocks);
+    free(spinLocks);
 
     char title[65] = "list-";
 
@@ -343,7 +377,7 @@ int main(int argc, char *argv[]) {
     totalWaitTime = totalWaitTime/totalOp;
 
     fprintf(stdout, "%s,%d,%d,%d,%d,%lld,%lld,%lld,%lld\n", title, numThreads, 
-            numIterations, 1, totalOp, runTime, timePerOperation, 
+            numIterations, numLists, totalOp, runTime, timePerOperation, 
             operationsPerSec, totalWaitTime);
 
     exit(SUCCESS);
--- p2b/lab2_plotter.gp
+++ p2b/lab2_plotter.gp
@@ -25,7 +25,7 @@ set terminal png
 set datafile separator ","
 
 # plotting thread number vs operations per second
-set title "List: Operations per Second vs. Thread Count"
+set title "Operations per Second vs. Thread Count"
 set xlabel "Thread Count"
 set xrange [0.75:32]
 set logscale x 2
@@ -38,3 +38,18 @@ plot \
     title 'spin lock' with linespoints lc rgb 'blue', \
     "< grep 'list-none-m,[0-24]*,1000,' lab2_list.csv" using ($2):($8) \
     title 'mutex lock' with linespoints lc rgb 'red', 
+
+# Plotting wait for lock and avg time per operation vs number of threads
+set title "Lock Wait Time and Time per Operation vs. Thread Count"
+set xlabel "Thread Count"
+set xrange [0.75:32]
+set logscale x 2
+set ylabel "Operations per Second"
+set logscale y 10
+set output 'lab2b_1.png'
+
+plot \
+    "< grep 'list-none-m,[0-24]*,1000,' lab2_list.csv" using ($2):($9) \
+    title 'lock wait time' with linespoints lc rgb 'blue', \
+    "< grep 'list-none-m,[0-24]*,1000,' lab2_list.csv" using ($2):($7) \
+    title 'time per operation' with linespoints lc rgb 'red', 
