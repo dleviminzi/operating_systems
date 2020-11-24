ace0b5471c09bb1b3076fd92b227beabd585b8e9
Daniel Levi-Minzi <dleviminzi@gmail.com>
2020-11-24 14:28:20 -0500

    fucking flashed the bone without uploading >:(

--- p4b/lab4b.c
+++ p4b/lab4b.c
@@ -1,4 +1,6 @@
 #include <mraa.h>
+#include <mraa/gpio.h>
+#include <mraa/aio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -13,18 +15,32 @@
 #define TRUE 1
 #define FALSE 0
 #define ERROR 1
+#define SUCCESS 0
+
+#define BUFF_SIZE 256
 
 mraa_aio_context tempSensor;
 mraa_gpio_context button;
 const int B = 4275;               // B value of the thermistor
 const int R0 = 100000;
 
-char *scale;      /* temp in f or c */
+char scale;      /* temp in f or c */
 int logFD;            /* file descriptor of logfile */
-int shutdown;
+int off;
+int period;
+int reporting;
 
+void printTime() {
+    time_t raw;
+    struct tm* Time;
+    time(&raw);
+    Time = localtime(&raw);
 
-struct pollfd pollArr[1]; /* specifying events to watch for */
+    fprintf(stdout, "%.2d:%.2d:%.2d ", Time->tm_hour, Time->tm_min, Time->tm_sec);
+    if (logFD != -1) {
+        dprintf(logFD, "%.2d:%.2d:%.2d ", Time->tm_hour, Time->tm_min, Time->tm_sec);
+    } 
+}
 
 
 /* redirects newFD to targetFD */
@@ -50,14 +66,11 @@ float calcTemp(float temp, char *scale) {
 }
 
 void reportTemp(float convTemp) {
-    time_t raw;
-    struct tm* Time;
-    time(&raw);
-    Time = localtime(&raw);
+    printTime();
 
-    fprintf(stdout, "%.2d:%.2d:%.2d %.1f\n", Time->tm_hour, Time->tm_min, Time->tm_sec, convTemp);
-    if (logFD != -1) {
-        dprintf(logFD, "%.2d:%.2d:%.2d %.1f\n", Time->tm_hour, Time->tm_min, Time->tm_sec, convTemp);
+    fprintf(stdout, "%.1f\n", convTemp);
+    if (logFD != -1 && reporting) {
+        dprintf(logFD, "%.1f\n", convTemp);
     }
 }
 
@@ -65,7 +78,7 @@ void reportTemp(float convTemp) {
 void getTemp(void *period) {
     int period = *((int *) period);
 
-    while (!shutdown) {
+    while (!off) {
         float temp = mraa_aio_read_float(tempSensor);
         float convTemp = calcTemp(temp, scale);
         reportTemp(convTemp);
@@ -75,15 +88,57 @@ void getTemp(void *period) {
     pthread_exit(NULL);
 }
 
-char *timeNow() {
+void shutdown() {
+    printTime();
 
+    fprintf(stdout, "SHUTDOWN\n");
+    if (logFD != -1) {
+        dprintf(logFD, "SHUTDOWN\n");
+    } 
+
+    mraa_aio_close(tempSensor);
+    mraa_gpio_close(button);
+    close(logFD);
+
+    exit(SUCCESS);
 }
+
+void command(char *cmd) {
+    if (strcmp(cmd, "SCALE=F") == 0) {
+        scale = 'F';
+    }
+    else if (strcmp(cmd, "SCALE=C") == 0) {
+        scale = 'C';
+    }
+    else if (strncmp(cmd, "PERIOD=", 7) == 0) {
+        period = (int) atoi(cmd+7);
+    }
+    else if (strcmp(cmd, "STOP") == 0) {
+        reporting = FALSE;
+    }
+    else if (strcmp(cmd, "START") == 0) {
+        reporting = TRUE;
+    }
+    else if (strcmp(cmd, "OFF") == 0) {
+        shutdown();
+    }
+    else if (!(strncmp(cmd, "LOG", 3) == 0)) {
+        fprintf(stderr, "Command not valid");
+        exit(ERROR);
+    }
+}
+
 
 int main(int argc, char* argv[]) {
     scale = 'F';
-    int period = 1;         /* seconds to wait between reading temp */         
+    period = 1;         /* seconds to wait between reading temp */         
     logFD = -1;
-    shutdown = FALSE;
+    off = FALSE;
+    reporting = FALSE;
+
+    struct pollfd inputPoll; /* specifying events to watch for */
+
+    char buff[BUFF_SIZE];
 
     /* getopt variables and options */
     int opt = 0;           
@@ -110,6 +165,8 @@ int main(int argc, char* argv[]) {
                 scale = optarg[0];
                 break;
             case 'l':
+                reporting = TRUE;
+
                 if ((logFD = open(optarg, O_CREAT | O_RDWR | O_TRUNC, 0666)) <= 0) {
                     fprintf(stderr, "Error on output: %s \nProper use: --logfile=filename", strerror(errno));
                     exit(ERROR);
@@ -124,51 +181,84 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    pthread_t tempSensor;
-
     if ((tempSensor = mraa_aio_init(1)) == NULL) {
         mraa_deinit();
         fprintf(stderr, "Failed to initialize temperature sensor.");
         exit(ERROR);
     }
 
+    if ((button = mraa_gpio_init(60)) == NULL) {
+        mraa_deinit();
+        fprintf(stderr, "Failed to initialize temperature sensor.");
+        exit(ERROR);
+    }
+
+    mraa_gpio_dir(button, MRAA_GPIO_IN);
+
+    pthread_t tSensor;
+
     /* init attributes of threads */
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); 
 
-    pthread_create(&tempSensor, NULL, getTemp, &period);
+    pthread_create(&tSensor, NULL, getTemp, &period);
 
 
-    pollArr[0].fd = STDIN_FILENO;
-    pollArr[0].events = POLLIN | POLLERR | POLLHUP;
+    inputPoll.fd = STDIN_FILENO;
+    inputPoll.events = POLLIN | POLLERR | POLLHUP;
 
     while(1) {
         /* check for shutdown button */
         if (mraa_gpio_read(button)) {
-            shutdown = TRUE;
+            off = TRUE;
+
             int rc;
             if ((rc = pthread_join(tempSensor, NULL))) {  /* join temp thread */
                 fprintf(stderr, "Thread joining failed with error code: %d\n", rc);
                 exit(ERROR); 
             }
+
             shutdown();
         }
 
         /* perform polling */
-        int ret;
-        if ((ret = poll(pollArr, 1, 0) < 0) {
+        int events;
+        if ((events = poll(inputPoll, 1, 0)) < 0) {
             fprintf(stderr, "Polling failed");
             exit(ERROR);
         }
+        else if (events == 0) {
+            continue;
+        }
 
-        
+        int lenBuff
+        if (inputPoll.revents && POLLIN) {
+            if ((lenBuff = read(STDIN_FILENO, buff, BUFF_SIZE)) < 0) {
+                fprintf(stderr, "Failed to read from stdin: %s", strerror(errno));
+                exit(ERROR);
+            }
+        }
+        else if (lenBuff == 0) {
+            continue;
+        }
 
+        int start = 0;
+        int i;
+
+        for (i = 0; i < lenBuff; ++i) {
+            if (buff[i] == '\n') {
+                buff[i] = '\0';
+                dprintf(logFD, "%s\n", buff + i);
+                command(buff + i);
+                start = i + 1;
+            }
+        }
     }
 
+        mraa_aio_close(tempSensor);
+        mraa_gpio_close(button);
+        close(logFD);
 
-
-
-
-
+        exit(SUCCESS);
 }
