a24052fda5bd92568bd7c2e6a6ae01b244581285
Daniel Levi-Minzi <dleviminzi@gmail.com>
2020-11-24 15:39:20 -0500

    done

--- p4b/check.sh
+++ p4b/check.sh
@@ -1,7 +1,70 @@
 #!/bin/bash
 
+# bad arg
 echo | ./lab4b --shit &> /dev/null
 if [[ $? -ne 1 ]]
 then
     echo "bad arg not caught"
 fi 
+
+# log working
+./lab4b --period=1 --scale="F" --log="logfile" <<-EOF
+SCALE=C
+STOP
+START
+SCALE=F
+PERIOD=2
+OFF
+EOF
+
+if [[ $? -ne 0 ]]
+then 
+    echo "failed logging"
+fi 
+
+if [ ! -s logfile ] 
+then
+    echo "didn't create logfile"
+fi
+
+grep "SCALE=C" LOG &> /dev/null; \
+if [[ $? -ne 0 ]]
+then
+	echo "didn't properly log scale"
+fi
+
+grep "STOP" LOG &> /dev/null; \
+if [[ $? -ne 0 ]]
+then
+	echo "didn't properly log stop"
+fi
+
+grep "START" LOG &> /dev/null; \
+if [[ $? -ne 0 ]]
+then
+	echo "didn't properly log start"
+fi
+
+grep "SCALE=F" LOG &> /dev/null; \
+if [[ $? -ne 0 ]]
+then
+	echo "didn't properly log scale"
+fi
+
+grep "PERIOD=2" LOG &> /dev/null; \
+if [[ $? -ne 0 ]]
+then
+	echo "didn't properly log period"
+fi
+
+grep "OFF" LOG &> /dev/null; \
+if [[ $? -ne 0 ]]
+then
+	echo "didn't properly log off"
+fi
+
+grep "SHUTDOWN" LOG &> /dev/null; \
+if [[ $? -ne 0 ]]
+then
+	echo "didn't properly log SHUTDOWN"
+fi
