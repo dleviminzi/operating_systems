636c349331c742b5bfd22d36be747aab19b30cd5
Daniel Levi-Minzi <dleviminzi@gmail.com>
2020-12-19 13:29:55 -0500

    ffs

--- p4c/lab4c_tls.c
+++ p4c/lab4c_tls.c
@@ -1,306 +1,354 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <getopt.h>
-#include <string.h>
-#include <poll.h>
-#include <time.h>
-#include <sys/time.h>
-#include <math.h>
-#include <unistd.h>
 #include <mraa.h>
-#include <mraa/aio.h>
-#include <ctype.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <mraa/gpio.h>
+#include <mraa/aio.h> 
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <getopt.h>
 #include <fcntl.h>
-#include <sys/socket.h>
+#include <math.h>
+#include <pthread.h>
+#include <poll.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
 #include <openssl/ssl.h>
 
-#define A0 1
-#define NOTIFY_SERVER 1
+#define TRUE 1
+#define FALSE 0
+#define ERROR 1
+#define SUCCESS 0
+#define SERV 1
 
-int period_arg = 1;
-int report = 1;
-char scale_arg = 'F';
+#define BUFF_SIZE 256
 
-/* GLOBALS */
-time_t next_time = 0;
-struct tm *cur_time;
-struct timeval clk;
-FILE *file = 0;
+mraa_aio_context tempSensor;
+mraa_gpio_context button;
+const int B = 4275;               // B value of the thermistor
+const int R0 = 100000;
+//int tempSensor;
+//int button;
 
-mraa_aio_context temp_read; 
+char scale;      /* temp in f or c */
+int logFD;            /* file descriptor of logfile */
+int off;
+int reporting;
+int period;
 
+/* new stuff for 4c */
+char *id = "";
 int port = -1;
-int sock;
-char* id = "";
 char* host = "";
+int sock;
 
-struct hostent *server;
-struct sockaddr_in server_addr;
-
-// SSL structs
+struct sockaddr_in serv_addr;
+struct hostent* serv_host;
+SSL_CTX* new_cont = NULL;
 SSL *ssl = NULL;
-SSL_CTX* ssl_context = NULL;
 
-void print_error(char* message) {
-	fprintf(stderr, message);
-	exit(1);
+void prnt(char *toPrint, int serv) {
+    if (serv) {
+        char prn[200];
+        sprintf(prn, "%s\n", toPrint);
+        SSL_write(ssl, prn, strlen(prn) + 1);
+    }
+    fprintf(stderr, "%s\n", toPrint);
+    dprintf(logFD, "%s\n", toPrint);
 }
 
-// outputs to stdout and log file
-void output(char *val, int notify_server) {
-	if (notify_server) {
-		char out[200];
-		sprintf(out, "%s\n", val);
-		SSL_write(ssl, out, strlen(out) + 1);
-	}
-	fprintf(file, "%s\n", val);
-	fprintf(stderr, "%s\n", val);
-	fflush(file);
+
+/* redirects newFD to targetFD */
+void fdRedirect(int newFD, int targetFD) {
+    close(targetFD);
+    dup2(newFD, targetFD);
 }
 
-// get temperature now as a double (self-expanatory)
-double get_temperature() {
-	int b_val = 4275; // b value of thermister
-	float R0 = 100000.0; // 100k
-	int temperature = mraa_aio_read(temp_read);
-	float R = 1023.0/((float) temperature) - 1.0;
-	R = R * R0;
-	float ret = 1.0/(log(R/R0)/b_val + 1/298.15) - 273.15; // temperature conversion, from datasheet
-	return scale_arg == 'C' ? ret : (ret * 9)/5 + 32; 
+/* calculate the temperature as is done on the doc page */
+double calcTemp(int temp) {
+    double t = 1023.0/(double)temp - 1.0;
+
+    t = R0*t;
+
+    float outputTemp = 1.0/(log(t/R0)/B+1/298.15)-273.15;
+
+    /* convert if needed */
+    if (scale == 'F') {
+        outputTemp = outputTemp * 9/5 + 32;
+    }
+
+    return outputTemp;
 }
 
-// outputs a report containing a timestamp of the current time and current temperature
-void output_time_temp() {
-	if (gettimeofday(&clk, 0) < 0) {
-		print_error("Error: could not get time of day.\n");
-	}
-	if (report && clk.tv_sec >= next_time) {
-		double temp = get_temperature();
-		int t = temp * 10;
-		cur_time = localtime(&clk.tv_sec);
-		char out[200];
-		sprintf(out, "%02d:%02d:%02d %d.%1d\n", 
-			cur_time->tm_hour, 
-			cur_time->tm_min, 
-			cur_time->tm_sec, 
-			t/10, 
-			t%10);
-		sprintf(out, "%s", out);
-		SSL_write(ssl, out, strlen(out));
-		fprintf(stderr, "%s", out);
-		fprintf(file, "%s", out);
-		fflush(file);
-		next_time = clk.tv_sec + period_arg; 
-	}
+void reportTemp(float convTemp) {
+    time_t raw;
+    struct tm* Time;
+    time(&raw);
+    Time = localtime(&raw);
+
+    char toPrint[160];
+    sprintf(toPrint, "%.2d:%.2d:%.2d %.1f", Time->tm_hour, Time->tm_min, Time->tm_sec, convTemp);
+    prnt(toPrint, SERV);
 }
 
-// shuts down with timestamp
-void shutdown_time(){
-	cur_time = localtime(&clk.tv_sec);
-	char a[200];
-	sprintf(a, "%02d:%02d:%02d SHUTDOWN", 
-		cur_time->tm_hour, 
-    	cur_time->tm_min, 
-    	cur_time->tm_sec);
-	output(a, NOTIFY_SERVER);
-	SSL_shutdown(ssl);
-	SSL_free(ssl);
-	exit(0);
+/* thread function to gather temp and then wait */
+void *getTemp(void *prd) {
+    int *period = (int *) prd;
+
+    while (!off) {
+        float temp = mraa_aio_read(tempSensor);
+        float convTemp = calcTemp(temp);
+        reportTemp(convTemp);
+        sleep(*period);
+    }
+
+    float temp = mraa_aio_read(tempSensor);
+    float convTemp = calcTemp(temp);
+    reportTemp(convTemp);
+
+    pthread_exit(NULL);
 }
 
-// process input from stdin
+void shtdwn() {
+    time_t raw;
+    struct tm* Time;
+    time(&raw);
+    Time = localtime(&raw);
+
+    char toPrint[160];
+    sprintf(toPrint, "%.2d:%.2d:%.2d SHUTDOWN\n", Time->tm_hour, Time->tm_min, Time->tm_sec);
+    prnt(toPrint, 0);
+
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    mraa_aio_close(tempSensor);
+    mraa_gpio_close(button);
+    close(logFD);
+
+    exit(SUCCESS);
+}
+
+void command(char *cmd) {
+    if (strcmp(cmd, "SCALE=F") == 0) {
+        prnt(cmd, 0);
+        scale = 'F';
+    }
+    else if (strcmp(cmd, "SCALE=C") == 0) {
+        prnt(cmd, 0);
+        scale = 'C';
+    }
+    else if (strncmp(cmd, "PERIOD=", 7) == 0) {
+        prnt(cmd, 0);
+        period = (int) atoi(cmd+7);
+    }
+    else if (strcmp(cmd, "STOP") == 0) {
+        prnt(cmd, 0);
+        reporting = FALSE;
+    }
+    else if (strcmp(cmd, "START") == 0) {
+        prnt(cmd, 0);
+        reporting = TRUE;
+    }
+    else if (strcmp(cmd, "OFF") == 0) {
+        prnt(cmd, 0);
+        shtdwn();
+    }
+    else if (strncmp(cmd, "LOG", 3) == 0) {
+        prnt(cmd, 0);
+    }
+    else {
+        fprintf(stderr, "Command not valid");
+        exit(ERROR);
+    }
+}
+
 void handle_input(char *input) {
-	while(*input == '\t' || *input == ' ') {
-		input++;
-	}
-	char *period_string = strstr(input, "PERIOD=");
-	char *log_string = strstr(input, "LOG");
+    int ret = SSL_read(ssl, input, 256);
+    if (ret > 0) {
+        fprintf(stderr, "GOT BYTES");
+        input[ret] = 0;
+    }
+    
+    char *start = input;
 
-	if(strcmp(input, "SCALE=F") == 0) {
-		output(input, 0);
-		scale_arg = 'F'; 
-	} else if(strcmp(input, "SCALE=C") == 0) {
-		output(input, 0);
-		scale_arg = 'C'; 
-	} else if(strcmp(input, "START") == 0) {
-		output(input, 0);
-		report = 1;
-	} else if(strcmp(input, "STOP") == 0) {
-		output(input, 0);
-		report = 0;
-	} else if(strcmp(input, "OFF") == 0) {
-		output(input, 0);
-		shutdown_time();
-	} else if(period_string == input) {
-		char *in = input;
-		in = in + 7; 
-		if(*in != 0) {
-			int p = atoi(in);
-			while(*in != 0) {
-				if (!isdigit(*in)) {
-					return;
-				}
-				in++;
-			}
-			period_arg = p;
-		}
-		output(input, 0);
-	} else if (log_string == input) {
-		output(input, 0); 
-	}
+    while (start < &input[ret]) {
+        char *end = start;
+
+        while(start < &input[ret] && *end != '\n') {
+            end++;
+        }
+
+        *end = 0;
+
+        command(start);
+        start = &end[1];
+    }
 }
 
-// take input from server
-void server_input(char* input) {
-	int readin = SSL_read(ssl, input, 256); // use SSL_read instead of read
-	if (readin > 0) {
-		input[readin] = 0;
-	}
-	char *in = input;
-	while (in < &input[readin]) {
-		char* cur = in;
-		while (*cur != '\n' && cur < &input[readin]) {
-			cur++;
-		}
-		*cur = 0;
-		handle_input(in);
-		in = &cur[1];
-	}
-}
-
-void parse_options(int argc, char* argv[]) {
-	struct option options[] = {
-		{"period", required_argument, NULL, 'p'},
-		{"scale", required_argument, NULL, 's'},
-		{"log", required_argument, NULL, 'l'},
-		{"id", required_argument, NULL, 'i'},
-		{"host", required_argument, NULL, 'h'},
-		{0, 0, 0, 0}
-	};
-
-	int opt; 
-
-	while ((opt = getopt_long(argc, argv, "", options, NULL)) != -1) {
-		switch (opt) {
-			case 'p': 
-				period_arg = atoi(optarg);
-				break;
-			case 's':
-				if (optarg[0] == 'F' || optarg[0] == 'C') {
-					scale_arg = optarg[0];
-					break;
-				}
-			case 'l':
-				file = fopen(optarg, "w+");
-    			if(file == NULL) {
-	           		print_error("Error: invalid log file.\n");
-				}
-				break;
-			case 'i':
-				id = optarg;
-				break;
-			case 'h':
-				host = optarg;
-				break;
-			default:
-				print_error("Invalid options: see usage.\n");
-		}
-	}
-	// get port number
-	if (optind < argc) {
-		port = atoi(argv[optind]);
-		if (port <= 0) {
-			print_error("Usage error: port is invalid.\n");
-		}
-	}
-}
 
 int main(int argc, char* argv[]) {
-	parse_options(argc, argv);	
-	
-	if (strlen(id) != 9) {
-		print_error("Usage error: use 9 digit ID number.\n");
-	} 
-	if (strlen(host) == 0) {
-		print_error("Usage error: must include argmuement for host.\n");
-	}
-	if (file == 0) {
-		print_error("Usage error: must include argument for log.\n");
-	}
+    scale = 'F';
+    period = 1;         /* seconds to wait between reading temp */         
+    logFD = -1;
+    off = FALSE;
+    reporting = FALSE;
 
-	// create a socket
-	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		print_error("Error: could not create socket in client.\n");
-	}
-	// find host
-	if ((server = gethostbyname(host)) == NULL) {
-		fprintf(stderr, "Error: could not find host\n");
-	}
-	memset((void *) &server_addr, 0, sizeof(server_addr));
-	server_addr.sin_family = AF_INET;
-	memcpy((char *) &server_addr.sin_addr.s_addr, (char *) server->h_addr, server->h_length);
-	server_addr.sin_port = htons(port);
-	if (connect(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-      	print_error("Error: could not connect on client side (TCP).\n");
-	}
+    /* getopt variables and options */
+    int opt = 0;           
+    int option_index = 0; 
 
-	OpenSSL_add_all_algorithms();
-	SSL_load_error_strings();
- 	if (SSL_library_init() < 0) {
- 		print_error("Error: could not initialize SSL library.\n");
- 	}
-	if ((ssl_context = SSL_CTX_new(TLSv1_client_method())) == NULL ) {
-		fprintf(stderr, "Error: failed to get SSL context.\n");
-    	exit(2);
-	}
-	ssl = SSL_new(ssl_context);
-	if (ssl == NULL) {
-    	fprintf(stderr, "Error: failed to set up SSL.\n");
-    	exit(2);
-	}
-	if (!SSL_set_fd(ssl, sock)) {
-    	fprintf(stderr, "Error: failed to associate file descriptor with SSL.\n");
-    	exit(2);
-	}
-	if (SSL_connect(ssl) != 1) {
-    	fprintf(stderr, "Error: SSL Connection rejected.\n");
-    	exit(2);
-	}
+    static struct option long_options[] = {
+        {"period", required_argument, NULL, 'p'},
+        {"scale", required_argument, NULL, 's'},
+        {"log", required_argument, NULL, 'l'},
+        {"id", required_argument, NULL, 'i'},
+        {"host", required_argument, NULL, 'h'},
+        {"log", required_argument, NULL, 'l'},
+        {0,0,0,0}
+    };
 
-	// send ID terminated by a newline
-	char out[50];
-	sprintf(out, "ID=%s\n", id);
-	sprintf(out, "%s", out);
-	SSL_write(ssl, out, strlen(out));
-	fprintf(stderr, "%s", out);
-	fprintf(file, "%s", out);
-	fflush(file);
+    while ((opt = getopt_long(argc, argv, ":p:l", long_options, &option_index)) != -1) {
+        switch(opt) {
+            case 'p':
+                period = atoi(optarg);
+                break;
+            case 's':
+                switch(*optarg) {
+                    case 'c':
+                    case 'C':
+                        scale = 'C';
+                        break;
+                    case 'f':
+                    case 'F':
+                        scale = 'F';
+                        break;
+                    default:
+                        fprintf(stderr, "Scale must be either S or F. Try again.");
+                        exit(ERROR);
+                }
+            case 'l':
+                reporting = TRUE;
 
-	// initialize I/O
-	if((temp_read = mraa_aio_init(A0)) == NULL) {
-		fprintf(stderr, "Error: could not initialize AIO\n");
-    	mraa_deinit();
-    	exit(1);
-	}
+                if ((logFD = open(optarg, O_CREAT | O_RDWR | O_TRUNC, 0666)) <= 0) {
+                    fprintf(stderr, "Error on output: %s \nProper use: --logfile=filename", strerror(errno));
+                    exit(ERROR);
+                }
+                break;
+            case 'i':
+                id = optarg;
+                break;
+            case 'h':
+                host = optarg;
+                break;
+            case '?':
+                fprintf(stderr, "Unknown option. Permitted option: shell");
+                exit(ERROR);
+            case ':': 
+                fprintf(stderr, "Invalid argument provided for option."); 
+                exit(ERROR);
+        }
+    }
+    
+    /* collect port number */
+    if (optind < argc) {
+        port = atoi(argv[optind]);
+        if (port <= 0) {
+            fprintf(stderr, "Port number must be greater than 0");
+            exit(ERROR);
+        }
+    }
 
-	struct pollfd poll_in; 
-	poll_in.events = POLLIN; 
-   	poll_in.fd = sock; 
+    /* check that mandatory options were given/valid */
+    if (strlen(host) == 0 || strlen(id) != 9 || logFD == 0) {
+        fprintf(stderr, "Invalid or missing mandatory arguments: host, id, log");
+        exit(ERROR);
+    }
 
-	char input[256];
+    /* connect to server via TCP */
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        fprintf(stderr, "Could not initiate socket.");
+        exit(ERROR);
+    }
+    else {
+        serv_host = gethostbyname(host);
+        
+        if (serv_host == NULL) {
+            fprintf(stderr, "Host was not found.");
+        }
+    }
 
-	while(1) {
-  		output_time_temp();
-		poll(&poll_in, 1, 0);
-		if (poll_in.revents & POLLIN) {
-			server_input(input);
-		}
-	}
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)(serv_host->h_addr), (char *)&serv_addr.sin_addr.s_addr, serv_host->h_length);
+    serv_addr.sin_port = htons(port);
 
-	mraa_aio_close(temp_read);
-   	exit(0);
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        fprintf(stderr, "Client failed to connect.");
+        exit(ERROR);
+    }
+
+    if (SSL_library_init() < 0) {
+        fprintf(stderr, "Could not load SSL library");
+    }
+    SSL_load_error_strings();
+    OpenSSL_add_all_algorithms();
+
+    new_cont = SSL_CTX_new(TLSv1_client_method());
+    if (new_cont == NULL) {
+        fprintf(stderr, "Failed to load SSL context");
+        exit(2);
+    }
+    ssl = SSL_new(new_cont);
+
+    if (ssl == NULL || !SSL_set_fd(ssl, sock) || SSL_connect(ssl) != 1) {
+        fprintf(stderr, "Failed to initialize SSL properly");
+        exit(2);
+    }
+
+    char out[80];
+    sprintf(out, "ID=%s", id);
+    prnt(out, SERV);
+
+    if ((tempSensor = mraa_aio_init(1)) == NULL) {
+        mraa_deinit();
+        fprintf(stderr, "Failed to initialize temperature sensor.");
+        exit(ERROR);
+    }
+
+    pthread_t tSensor;
+
+    /* init attributes of threads */
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); 
+
+    pthread_create(&tSensor, NULL, getTemp, &period);
+
+    /* poll for input from server */
+    struct pollfd pIn;
+    pIn.fd = sock;
+    pIn.events = POLLIN;
+
+    char input[256];
+    
+    while(1) {
+        poll(&pIn, 1, 0);
+        if (pIn.revents & POLLIN) {
+            handle_input(input);
+        }
+    }
+
+    int rc;
+    if ((rc = pthread_join(tSensor, NULL))) {  /* join temp thread */
+        fprintf(stderr, "Thread joining failed with error code: %d\n", rc);
+        exit(ERROR); 
+    }
+
+    mraa_aio_close(tempSensor);
+    close(logFD);
+
+    exit(SUCCESS);
 }
